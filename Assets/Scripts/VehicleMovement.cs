0c46eabff6a9456da920d694a98c4f3cc974c99d
AzafTechnologies <azafsathar0011@gmail.com>
2026-09-19 17:34:05 +0530

    Integrate Input System into VehicleMovement
    Add Unity Input System support to VehicleMovement: import InputSystem, add moveAction (InputAction), moveInput (Vector2) and turnSpeed fields, enable the action in Start and read the move vector in Update. Scene serialized the Move action with WASD/arrow bindings and also updated the object's local position/rotation. Note: moveInput is read and stored but not yet applied to steering (movement still uses speed/turnSpeed).

--- Assets/Scripts/VehicleMovement.cs
+++ Assets/Scripts/VehicleMovement.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VehicleMovement : MonoBehaviour
 {
     public float speed;
+
     public float turnSpeed;
+
+    public InputAction moveAction;
+
+    public Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveInput = moveAction.ReadValue<Vector2>();
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
         transform.Translate(Vector3.right * turnSpeed * Time.deltaTime);
     }
 }
